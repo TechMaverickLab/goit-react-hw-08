0678bec6e25888fcef7e71f1e415dc3726811ea9
TechMaverickLab <TechMaverickLab@gmail.com>
2024-04-24 00:21:48 -0400

    transported main

--- main.jsx
+++ main.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import axios from 'axios';
 import { BrowserRouter } from 'react-router-dom';
-import App from '../src/components/App';
-import './index.css';
+import App from './src/components/App';
+import './src/index.css';
 import { Provider } from 'react-redux'; 
-import { store, persistor } from './redux/store'; 
+import { store, persistor } from './src/redux/store'; 
 import { PersistGate } from 'redux-persist/integration/react';
 
 const token = localStorage.getItem('token');
